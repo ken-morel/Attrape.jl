d35751697a92c673cd272ab8bac70bd5c89a8d7c
Engon Ken Morel <engonken8@gmail.com>
2025-08-07 23:29:21 +0100

    implemented synced window navigation

--- examples/hello_world.jl
+++ examples/hello_world.jl
@@ -2,13 +2,27 @@ include("../src/Attrape.jl")
 using .Attrape
 import Mousetrap
 
-Home = Attrape.BuilderPage() do ctx
-    return Mousetrap.Button()
+
+Page1 = Attrape.BuilderPage() do ctx
+    button = "Hello world 1(click to go to next page)" |> Mousetrap.Label |> Mousetrap.Button
+    Mousetrap.connect_signal_clicked!(button) do _
+        push!(ctx, Page2; replace = true)
+        return
+    end
+    return button |> Mousetrap.Frame
+end
+Page2 = Attrape.BuilderPage() do ctx
+    button = "Hello again world 2(click to go to previous page)" |> Mousetrap.Label |> Mousetrap.Button
+    Mousetrap.connect_signal_clicked!(button) do _
+        push!(ctx, Page1; replace = true)
+        return
+    end
+    return button |> Mousetrap.Frame
 end
 
 
 function create_application()
-    return application("cm.rbs.engon.attrape"; home = Home)
+    return application("cm.rbs.engon.attrape"; home = Page1)
 end
 
 function (@main)(::Vector{String})
--- src/page.jl
+++ src/page.jl
@@ -1,5 +1,7 @@
 abstract type AbstractPage end
 
+const PageBuilder = Function
+
 struct PageBuildContext
     app::AbstractApplication
     window::AbstractWindow
@@ -14,3 +16,34 @@ end
 isrebuildable(::BuilderPage) = false
 
 render(b::BuilderPage, ctx::PageBuildContext) = b.builder(ctx)
+
+
+function Base.push!(
+        ctx::PageBuildContext, builder::PageBuilder;
+        args...,  # just to be explicit, builder is also a Function
+    )
+    page = builder(ctx)
+    if isnothing(page.builder)
+        page.builder = builder
+        page.rebuildable = true
+    end
+    return if !Efus.iserror(page)
+        push!(ctx.window.router, page; args...)
+    else
+        Efus.display(page)
+        throw(page)
+    end
+end
+Base.push!(builder::Function, ctx::PageBuildContext; args...) =
+    push!(ctx, builder; args...)
+
+function Base.push!(
+        ctx::PageBuildContext, page::AbstractPage;
+        args...,  # just to be explicit, builder is also a Function
+    )
+    return push!(ctx.window.router, page; args...)
+end
+
+function Base.pop!(ctx::PageBuildContext; args...)
+    return pop!(ctx.window.router; args...)
+end
