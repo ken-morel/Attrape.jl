b87642cb4508cae8e5864705d54ffcdd3fbc90e0
ken-morel <engonken8@gmail.com>
2025-09-23 23:17:38 +0100

    improved on test script

--- test.jl
+++ test.jl
@@ -9,13 +9,13 @@ const slider_value = Reactant(0.0)
 const switch_active = Reactant(false)
 
 const home_page = Page(page"""
-    Box box=V margin="10 10"
+    Box box=V margin="10x10"
         Label text="Welcome to the Widget Showcase!" halign=center expand=true
         Label text="Use the navigation on the left to explore the widgets." halign=center expand=true
 """)
 
 const text_page = Page(page"""
-    Box box=V margin="10 10"
+    Box box=V margin="10x10"
         Label text="Entry (1-line) and Label, reactively linked:"
         Entry text=text_reactant
         Label text=(text_reactant')
@@ -24,13 +24,13 @@ const text_page = Page(page"""
 """)
 
 const controls_page = Page(page"""
-    Box box=V margin="10 10" expand=true
-        Box box=H margin="5 5"
+    Box box=V margin="10x10" expand=true
+        Box box=H margin="5x5"
             Label text="Switch:"
             Switch active=switch_active
             Label text="Spinner (reacts to Switch):"
             Spinner active=switch_active
-        Box box=V margin="5 5"
+        Box box=V margin="5x5"
             Label text=(slider_value')
             Slider value=slider_value min=0 max=1 step=0.01
             Label text="ProgressBar (reacts to Slider):"
@@ -40,7 +40,7 @@ const controls_page = Page(page"""
 # I am assuming this path exists on a standard Linux system with the Julia icon.
 # If not, the user can change it to a valid image path.
 const media_page = Page(page"""
-    Box box=V margin="10 10" halign=center valign=center expand=true
+    Box box=V margin="10x10" halign=center valign=center expand=true
         Label text="Picture widget:"
         Picture source="/usr/share/icons/hicolor/48x48/apps/julia.png" size="48x48"
 """)
@@ -53,7 +53,7 @@ navigate_media(_) = push!(ROUTER, media_page)
 Application("com.julia.widget-showcase") do ctx
     page"""
         Box box=H
-            Box box=V size="150x0" margin="5 5"
+            Box box=V size="150x0" margin="5x5"
                 Button text="Home" onclick=navigate_home
                 Button text="Text Widgets" onclick=navigate_text
                 Button text="Controls" onclick=navigate_controls
